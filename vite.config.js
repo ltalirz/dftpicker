9bef29a27d9d8f6dac3401ea629170f644ceb345
Leopold Talirz <leopold.talirz@gmail.com>
2025-04-21 09:55:03 +0200

    fix: relative paths

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dftpicker/', // Set the base URL for GitHub Pages
-  resolve: {
-    extensions: ['.js', '.jsx', '.json']
-  }
+  base: '/dftpicker/',
 })
